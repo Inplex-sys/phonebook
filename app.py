159fba6f21e4077e9ede01c42e8a9bf3a95b5594
Inplex-sys <Inplex-sys+VBTuMirEZrscYlFx@protonmail.ch>
2023-07-03 11:45:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,3 @@
-################################################################################
-#                          Copyrights and license                              #
-################################################################################
-#                                                                              #
-# Copyright 2021 Inplex-sys <Inplex-sys@protonmail.ch>                         #
-#                                                                              #
-# This file is part of DarkUtilities.                                          #
-#                                                                              #
-# DarkUtilities is list of software: you can redistribute it and/or modify     #
-# it under the terms of the GNU Lesser General Public License as published by  #
-# the Free Software Foundation, either version 3 of the License, or            #
-# (at your option) any later version.                                          #
-#                                                                              #
-# DarkUtilities is distributed in the hope that it will be useful, but WITHOUT #
-# ANY  WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
-# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  #
-# for more details.                                                            #
-#                                                                              #
-# You should DarkUtilities received a copy of the GNU Lesser General Public    #
-# License.                                                                     #
-# along with DarkUtilities. If not, see <http://www.gnu.org/licenses/>.        #
-#                                                                              #
-################################################################################
-#                     Made with ❤️ by github.com/Inplex-sys                    #
-################################################################################
-
 from flask import *
 import sqlite3
 import json
